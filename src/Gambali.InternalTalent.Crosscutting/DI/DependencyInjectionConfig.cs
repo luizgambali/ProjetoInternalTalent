e2b68c88ee46b3693209335ca6d54dccb5e186ef
Luiz Gambali <luiz.michel.gambali@avanade.com>
2022-08-12 16:14:38 -0300

    feat: add CrossCutting project, with DependencyInjection resolve class

---
+++ src/Gambali.InternalTalent.Crosscutting/DI/DependencyInjectionConfig.cs
@@ -0,0 +1,27 @@
+﻿using Gambali.InternalTalent.Application.Service;
+using Gambali.InternalTalent.Application.Service.Interface;
+using Gambali.InternalTalent.Domain.Interfaces;
+using Gambali.InternalTalent.Infra.DatabaseContext;
+using Gambali.InternalTalent.Infra.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gambali.InternalTalent.WebApi.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<ApplicationDbContext>();
+
+            services.AddScoped<IAlunoRepository, AlunoRepository>();
+            services.AddScoped<ICursoRepository, CursoRepository>();
+            services.AddScoped<IMatriculaRepository, MatriculaRepository>();
+
+            services.AddScoped<IAlunoService, AlunoService>();
+            services.AddScoped<ICursoService, CursoService>();
+            services.AddScoped<IMatriculaService, MatriculaService>();
+
+            return services;
+        }
+    }
+}
